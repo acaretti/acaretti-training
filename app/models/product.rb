0782bec034f81ee2be75bb870d0cef15e5413702
Adrian Caretti <acaretti@timbasoftware.com>
2012-06-04 10:31:41 -0300

    Test working

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,8 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :image, :name, :price
+    
+  validates :name, :description, :image, :presence => true
+  validates_uniqueness_of(:name)
+  validates_numericality_of :price, :greater_than_or_equal_to => 0.01
+  validates_format_of :image, :with => /^(http|https).*(gif|png|jpg)$/
 end
--- test/functional/products_controller_test.rb
+++ test/functional/products_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class ProductsControllerTest < ActionController::TestCase
   setup do
-    @product = products(:one)
+    @product = products(:three)
   end
 
   test "should get index" do
@@ -18,7 +18,7 @@ class ProductsControllerTest < ActionController::TestCase
 
   test "should create product" do
     assert_difference('Product.count') do
-      post :create, product: { description: @product.description, image: @product.image, name: @product.name, price: @product.price }
+      post :create, product: { description: 'A Description', image: 'http.image.gif', name: 'Name', price: 9.99 }
     end
 
     assert_redirected_to product_path(assigns(:product))
@@ -45,5 +45,51 @@ class ProductsControllerTest < ActionController::TestCase
     end
 
     assert_redirected_to products_path
+  end
+  
+  test "name should not be empty" do
+    assert_no_difference('Product.count') do
+      post :create, product: {description: 'Description', image: 'http.image.gif', name: '', price: 9.99}
+    end
+  end
+  
+  test "description should not be empty" do
+    assert_no_difference('Product.count') do
+      post :create, product: {description: '', image: 'http.image.gif', name: 'Name2', price: 9.99}
+    end
+  end
+  
+  test "image should not be empty" do
+    assert_no_difference('Product.count') do
+      post :create, product: {description: 'Description', image: '', name: 'Name2', price: 9.99}
+    end
+  end
+  
+  test "price must be positive" do
+    assert_no_difference('Product.count') do
+      post :create, product: {description: 'Description', image: 'http.image.gif', name: 'Name2', price: -1}
+    end
+  end
+
+  test "image must be .gif .png .jpg" do
+    assert_difference('Product.count') do
+      post :create, product: {description: 'Description', image: 'http.image.gif', name: 'Name.gif', price: 1}
+    end
+    assert_difference('Product.count') do
+      post :create, product: {description: 'Description', image: 'http.image.png', name: 'Name.png', price: 1}
+    end
+    assert_difference('Product.count') do
+      post :create, product: {description: 'Description', image: 'http.image.jpg', name: 'Name.jpg', price: 1}
+    end
+    assert_no_difference('Product.count') do
+      post :create, product: {description: 'Description', image: 'http.image.cualquiercosa', name: 'Name.cualquiercosa', price: 1}  
+    end
+  end
+  
+  test "name must be unique" do
+    post :create, product: {description: 'Description', image: 'http.image.gif', name: 'Name Test', price: 2}
+    assert_no_difference('Product.count') do
+      post :create, product: {description: 'Other Description', image: 'http.Otherimage.gif', name: 'Name Test', price: 4}
+    end
   end
 end
